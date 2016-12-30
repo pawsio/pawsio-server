82624d50f8587ec0247e7079d647dcf0f39965f1
Christopher Bruner <chrbruner9@gmail.com>
2016-12-30 12:34:32 -0800

    Added boilerplate server for handling users and a test route

---
+++ server.js
@@ -0,0 +1,11 @@
+require('dotenv').load();
+const app = require('./lib/app');
+const http = require('http');
+const port = process.env.PORT || 3000;
+require('./lib/setup-mongoose');
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+    console.log('Server listening to', server.address().port);
+});
